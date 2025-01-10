8f0934cba45981b5c318f3b3dc4b6575c70e583f
Piotr Janczura <piotr@janczura.pl>
2025-01-10 14:14:30 +0100

    Struktura z kontrolerem domyślnym i modułem Person.

---
+++ PersonelFiles/src/Default/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Default\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'default_controller')]
+    public function index(): Response
+    {
+        return $this->render('Default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
